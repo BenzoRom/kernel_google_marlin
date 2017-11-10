da17968804ac9f907988fcee67f1dc9585c0f56d
Todd Kjos <tkjos@google.com>
2017-11-10 15:30:27 -0800

    android: binder: fix proc->files use-after-free
    (from https://patchwork.kernel.org/patch/10058587/)
    
    proc->files cleanup is initiated by binder_vma_close. Therefore
    a reference on the binder_proc is not enough to prevent the
    files_struct from being released while the binder_proc still has
    a reference. This can lead to an attempt to dereference the
    stale pointer obtained from proc->files prior to proc->files
    cleanup. This has been seen once in task_get_unused_fd_flags()
    when __alloc_fd() is called with a stale "files".
    
    The fix is to always use get_files_struct() to obtain struct_files
    so that the refcount on the files_struct is used to prevent
    a premature free. proc->files is removed since we get it every
    time.
    
    Bug: 69164715
    Change-Id: I6431027d3d569e76913935c21885201505627982
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -328,10 +328,9 @@ struct binder_ref {
 };
 
 enum binder_deferred_state {
-	BINDER_DEFERRED_PUT_FILES    = 0x01,
-	BINDER_DEFERRED_FLUSH        = 0x02,
-	BINDER_DEFERRED_RELEASE      = 0x04,
-	BINDER_ZOMBIE_CLEANUP        = 0x08,
+	BINDER_DEFERRED_FLUSH        = 0x01,
+	BINDER_DEFERRED_RELEASE      = 0x02,
+	BINDER_ZOMBIE_CLEANUP        = 0x04,
 };
 
 struct binder_seq_head {
@@ -373,7 +372,6 @@ struct binder_proc {
 	int pid;
 	int active_thread_count;
 	struct task_struct *tsk;
-	struct files_struct *files;
 	struct files_struct *zombie_files;
 	struct hlist_node deferred_work_node;
 	int deferred_work;
@@ -553,22 +551,34 @@ static inline void binder_queue_for_zombie_cleanup(struct binder_proc *proc);
 static void binder_put_thread(struct binder_thread *thread);
 static struct binder_thread *binder_get_thread(struct binder_proc *proc);
 
+struct files_struct *binder_get_files_struct(struct binder_proc *proc)
+{
+	return get_files_struct(proc->tsk);
+}
+
 static int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
 {
-	struct files_struct *files = proc->files;
+	struct files_struct *files;
 	unsigned long rlim_cur;
 	unsigned long irqs;
+	int ret;
 
+	files = binder_get_files_struct(proc);
 	if (files == NULL)
 		return -ESRCH;
 
-	if (!lock_task_sighand(proc->tsk, &irqs))
-		return -EMFILE;
+	if (!lock_task_sighand(proc->tsk, &irqs)) {
+		ret = -EMFILE;
+		goto err;
+	}
 
 	rlim_cur = task_rlimit(proc->tsk, RLIMIT_NOFILE);
 	unlock_task_sighand(proc->tsk, &irqs);
 
-	return __alloc_fd(files, 0, rlim_cur, flags);
+	ret = __alloc_fd(files, 0, rlim_cur, flags);
+err:
+	put_files_struct(files);
+	return ret;
 }
 
 /*
@@ -577,8 +587,12 @@ static int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
 static void task_fd_install(
 	struct binder_proc *proc, unsigned int fd, struct file *file)
 {
-	if (proc->files)
-		__fd_install(proc->files, fd, file);
+	struct files_struct *files = binder_get_files_struct(proc);
+
+	if (files) {
+		__fd_install(files, fd, file);
+		put_files_struct(files);
+	}
 }
 
 /*
@@ -586,18 +600,20 @@ static void task_fd_install(
  */
 static long task_close_fd(struct binder_proc *proc, unsigned int fd)
 {
+	struct files_struct *files = binder_get_files_struct(proc);
 	int retval;
 
-	if (proc->files == NULL)
+	if (files == NULL)
 		return -ESRCH;
 
-	retval = __close_fd(proc->files, fd);
+	retval = __close_fd(files, fd);
 	/* can't restart close syscall because file table entry was cleared */
 	if (unlikely(retval == -ERESTARTSYS ||
 		     retval == -ERESTARTNOINTR ||
 		     retval == -ERESTARTNOHAND ||
 		     retval == -ERESTART_RESTARTBLOCK))
 		retval = -EINTR;
+	put_files_struct(files);
 
 	return retval;
 }
@@ -4104,7 +4120,6 @@ static void binder_vma_close(struct vm_area_struct *vma)
 		     (vma->vm_end - vma->vm_start) / SZ_1K, vma->vm_flags,
 		     (unsigned long)pgprot_val(vma->vm_page_prot));
 	binder_alloc_vma_close(&proc->alloc);
-	binder_defer_work(proc, BINDER_DEFERRED_PUT_FILES);
 }
 
 static int binder_vm_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
@@ -4147,10 +4162,7 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 
 	ret = binder_alloc_mmap_handler(&proc->alloc, vma);
 
-	if (!ret) {
-		proc->files = get_files_struct(current);
-		return 0;
-	}
+	return ret;
 
 err_bad_arg:
 	pr_err("binder_mmap: %d %lx-%lx %s failed %d\n",
@@ -4395,8 +4407,6 @@ static void binder_deferred_release(struct binder_proc *proc)
 	struct rb_node *n;
 	int threads, nodes, incoming_refs, outgoing_refs, active_transactions;
 
-	BUG_ON(proc->files);
-
 	binder_proc_lock(proc, __LINE__);
 	binder_queue_for_zombie_cleanup(proc);
 	binder_proc_unlock(proc, __LINE__);
@@ -4626,18 +4636,6 @@ static void binder_deferred_func(struct work_struct *work)
 			defer = 0;
 		}
 		mutex_unlock(&binder_deferred_lock);
-
-		if (defer & BINDER_DEFERRED_PUT_FILES) {
-			binder_proc_lock(proc, __LINE__);
-			if (proc->files) {
-				BUG_ON(proc->zombie_files);
-				proc->zombie_files = proc->files;
-				proc->files = NULL;
-				binder_queue_for_zombie_cleanup(proc);
-				defer |= BINDER_ZOMBIE_CLEANUP;
-			}
-			binder_proc_unlock(proc, __LINE__);
-		}
 
 		if (defer & BINDER_DEFERRED_FLUSH)
 			binder_deferred_flush(proc);
